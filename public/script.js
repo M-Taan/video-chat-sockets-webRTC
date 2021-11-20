ccabd3b27eaa878647edeef18645c274b7f5dd65
Mohamad Taan <mohamadmtaan@gmail.com>
2021-11-20 12:57:00 +0200

    added some comments

--- public/script.js
+++ public/script.js
@@ -19,8 +19,15 @@ navigator.mediaDevices
     audio: true,
   })
   .then((stream) => {
+    // capture my own video
     startVideo(ownVideo, stream);
 
+    // when new user is connected fire the function connectWithUser
+    socket.on("new-connected-user", (userId) => {
+      connectWithUser(userId, stream);
+    });
+
+    // event call to answer and listen
     peer.on("call", (call) => {
       call.answer(stream);
       const video = document.createElement("video");
@@ -28,37 +35,37 @@ navigator.mediaDevices
         startVideo(video, userVideoStream);
       });
     });
-
-    socket.on("new-connected-user", (userId) => {
-      connectWithUser(userId, stream);
-    });
   });
 
+// user disconnected
 socket.on("user-disconnected", (userId) => {
   connected_users[userId].close();
 });
 
+// client socket connection
 socket.on("connect", () => {
   console.log(socket.id);
 });
+
+// get a unique userID
 peer.on("open", (id) => {
   socket.emit("join", Room_Id, id);
 });
 
+// connect with other users
 function connectWithUser(userId, stream) {
   const call = peer.call(userId, stream);
   const video = document.createElement("video");
   call.on("stream", (userVideoStream) => {
     startVideo(video, userVideoStream);
   });
-
   call.on("close", () => {
     video.remove();
   });
-
   connected_users[userId] = call;
 }
 
+// play video
 function startVideo(video, stream) {
   video.srcObject = stream;
   video.addEventListener("loadedmetadata", () => {
--- server.js
+++ server.js
@@ -6,23 +6,32 @@ const httpServer = createServer(app);
 const io = new Server(httpServer);
 const { v4: uuidV4 } = require("uuid");
 
+// set view engine
 app.set("view engine", "ejs");
+// set static path
 app.use(express.static("public"));
 
+// intial view, redirect to room with id using uuid
 app.get("/", (req, res) => {
   res.redirect(`/${uuidV4()}`);
 });
 
+// enter room with generated id
 app.get("/:room", (req, res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+// start socket connection
 io.on("connection", (socket) => {
   console.log("User is connected");
+  // join room event, it takes back userID and roomID
   socket.on("join", (roomId, userId) => {
+    // join the room
     console.log(`room id is ${roomId} and userId is ${userId}`);
     socket.join(roomId);
+    // when new user enters start the event new-connected-user
     socket.broadcast.to(roomId).emit("new-connected-user", userId);
+    // when new user exits start the event user-disconnected
     socket.on("disconnect", () => {
       socket.broadcast.to(roomId).emit("user-disconnected", userId);
     });
